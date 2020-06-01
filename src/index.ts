45244e801c732df2ade8492ca947a16e42168bf4
Dmitri Lazarev <7235200@gmail.com>
2020-06-01 07:31:07 +0200

    ugly dom

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,35 @@
 import './main.css';
 import Grid, { TSource } from './Grid';
 
-const source: TSource = [
-  [0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-  [1, 0, 0, 0, 1, 1, 0, 1, 0, 0, 1, 0, 0, 0, 1, 0, 1, 1],
-  [1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 1],
-  [1, 0, 1, 0, 1, 1, 0, 0, 1, 0, 1, 1, 0, 1, 0, 0, 1, 1],
-  [1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 1, 0, 1, 0, 1, 0, 1],
-  [1, 0, 0, 0, 1, 1, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 1],
-  [1, 1, 1, 0, 1, 0, 0, 1, 1, 0, 1, 1, 0, 1, 0, 1, 0, 1],
-  [1, 0, 0, 0, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 0, 1, 0, 1],
-  [1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1],
-  [1, 0, 0, 0, 1, 0, 1, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 1],
-  [1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-  [1, 0, 1, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 1, 1, 0, 1, 1],
-  [1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 1, 0, 0, 0, 1],
-  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0],
-];
+const genRow = (size: number) => {
+  return new Array(size).fill(1).map(getRandom);
+};
 
-const grid = new Grid(source);
-const path = Array.from(grid.dfs() as Set<string>);
+const generateSource = (sWidth: number, sheight: number) => {
+  const width = sWidth;
+  let height = sheight;
+
+  const src: number[][] = [];
+
+  while (height >= 0) {
+    src.push(genRow(width));
+    height--;
+  }
+
+  src[0][0] = 0;
+  src[sheight - 1][sWidth - 1] = 0;
+
+  return src;
+};
+
+function getRandom() {
+  const num = Math.random();
+  if (num < 0.3) return 1;
+  else return 0
+}
 
 // @ts-ignore
-const root = document.getElementById('root');
+const root = document.getElementById('grid');
 
 const createNode = ([vX, vY, vZ]: number[]) => {
   // @ts-ignore
@@ -37,30 +44,57 @@ const createNode = ([vX, vY, vZ]: number[]) => {
   root.appendChild(node);
 };
 
-let x: number = 0;
+let source: TSource;
+let path: string[];
 
-while (x < source[0].length) {
-  let y: number = 0;
+const draw = () => {
+  root.textContent = '';
 
-  while (y < source.length) {
-    createNode([x, y, source[y][x]]);
-    y++;
+  source = generateSource(30, 30) as TSource;
+  const grid = new Grid(source);
+  path = Array.from(grid.dfs() as Set<string>);
+  let x: number = 0;
+
+  while (x < source[0].length) {
+    let y: number = 0;
+
+    while (y < source.length) {
+      createNode([x, y, source[y][x]]);
+      y++;
+    }
+
+    x++;
   }
+};
 
-  x++;
-}
+const delay = 85;
 
-const delay = 100;
+const run = () => {
+  path.forEach((id, idx) => {
+    setTimeout(() => {
+      // @ts-ignore
+      const current = document.getElementById(id);
+      // @ts-ignore
+      const prev = document.getElementById(path[idx - 1]);
 
-path.forEach((id, idx) => {
-  setTimeout(() => {
-    // @ts-ignore
-    const current = document.getElementById(id);
-    // @ts-ignore
-    const prev = document.getElementById(path[idx - 1]);
+      if (prev) prev.dataset.active = false;
+      current.dataset.active = true;
 
-    if (prev) prev.dataset.active = false;
-    current.dataset.active = true;
+      if (idx === path.length - 1) {
+        setTimeout(() => {
+          current.dataset.active = false;
+        }, 1000);
+      }
+    }, delay * idx);
+  });
+};
 
-  }, delay * idx);
-});
+// @ts-ignore
+const runButton = document.getElementById('run');
+// @ts-ignore
+const generateButton = document.getElementById('generate');
+
+runButton.onclick = run;
+generateButton.onclick = () => {
+  draw();
+};
